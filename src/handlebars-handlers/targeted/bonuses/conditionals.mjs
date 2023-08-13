5884dda369f5b0a393cc6e5bb97d72963fe59d53
dmrickey <dmrickey@gmail.com>
2023-08-13 09:48:54 -0500

    Working Conditionals input

--- src/handlebars-handlers/targeted/bonuses/conditionals.mjs
+++ src/handlebars-handlers/targeted/bonuses/conditionals.mjs
@@ -1,33 +1,129 @@
 import { MODULE_NAME } from "../../../consts.mjs";
 import { templates } from "../../init.mjs";
-import { addNodeToRollBonus } from "../../roll-bonus-on-actor-sheet.mjs";/**
+import { addNodeToRollBonus } from "../../roll-bonus-on-actor-sheet.mjs";
 
-/**
- *
- * @param {object} sourceObj
- * @param {object} targetObj
- * @param {string} keepPath
- * @returns
- */
-function keepUpdateArray(sourceObj, targetObj, keepPath) {
-    const newValue = getProperty(targetObj, keepPath);
-    if (newValue == null) return;
-    if (Array.isArray(newValue)) return;
+function getConditionalTargets() {
+    const result = {
+        attack: pf1.config.conditionalTargets.attack._label,
+        damage: pf1.config.conditionalTargets.damage._label,
+        size: pf1.config.conditionalTargets.size._label,
+        effect: pf1.config.conditionalTargets.effect._label,
+    };
 
-    const newArray = deepClone(getProperty(sourceObj, keepPath) || []);
-
-    for (const [key, value] of Object.entries(newValue)) {
-        if (foundry.utils.getType(value) === "Object") {
-            const subData = expandObject(value);
-            newArray[key] = mergeObject(newArray[key], subData);
-        } else {
-            newArray[key] = value;
-        }
+    // Only add Misc target if subTargets are available
+    if (Object.keys(getConditionalSubTargets("misc")).length > 0) {
+        result.misc = game.i18n.localize(pf1.config.conditionalTargets.misc._label);
     }
-
-    setProperty(targetObj, keepPath, newArray);
+    return result;
 }
 
+/**
+ * Generates lists of conditional subtargets this attack can have.
+ *
+ * @param {string} target - The target key, as defined in PF1.conditionTargets.
+ * @returns {Object<string, string>} A list of conditionals
+ */
+function getConditionalSubTargets(target) {
+    const result = {};
+    // Add static targets
+    if (hasProperty(pf1.config.conditionalTargets, target)) {
+        for (const [k, v] of Object.entries(pf1.config.conditionalTargets[target])) {
+            if (!k.startsWith("_") && !k.startsWith("~")) result[k] = v;
+        }
+    }
+    // Add subtargets depending on attacks
+    if (["attack", "damage"].includes(target)) {
+        // Add specific attacks
+        result["attack_0"] = `${game.i18n.localize("PF1.Attack")} 1`;
+        // for (const [k, v] of Object.entries(this.data.attackParts)) {
+        //   result[`attack_${Number(k) + 1}`] = v[1];
+        // }
+    }
+    // Add subtargets affecting effects
+    if (target === "effect") {
+        result["dc"] = game.i18n.localize("PF1.DC");
+    }
+    // Add misc subtargets
+    if (target === "misc") {
+        // Add charges subTarget with specific label
+        result["charges"] = game.i18n.localize("PF1.ChargeCost");
+    }
+    return result;
+}
+
+/** Generates lists of conditional modifier bonus types applicable to a formula.
+ * @param {string} target - The target key as defined in PF1.conditionTargets.
+ * @returns {Object.<string, string>} A list of bonus types.
+ */
+function getConditionalModifierTypes(target) {
+    const result = {};
+    if (target === "attack" || target === "damage") {
+        // Add bonusModifiers from PF1.bonusModifiers
+        for (const [k, v] of Object.entries(pf1.config.bonusModifiers)) {
+            result[k] = v;
+        }
+    }
+    if (target === "damage") {
+        for (const damageType of pf1.registry.damageTypes) {
+            result[damageType.id] = damageType.name;
+        }
+    }
+    return result;
+}
+
+/* Generates a list of critical applications for a given formula target.
+ * @param {string} target - The target key as defined in PF1.conditionalTargets.
+ * @returns {Object.<string, string>} A list of critical applications.
+ */
+function getConditionalCritical(target) {
+    let result = {};
+    // Attack bonuses can only apply as critical confirm bonus
+    if (target === "attack") {
+        result = { ...result, normal: "PF1.Normal", crit: "PF1.CriticalConfirmBonus" };
+    }
+    // Damage bonuses can be multiplied or not
+    if (target === "damage") {
+        result = {
+            ...result,
+            normal: "PF1.Normal",
+            crit: "PF1.CritDamageBonusFormula",
+            nonCrit: "PF1.NonCritDamageBonusFormula",
+        };
+    }
+    return result;
+}
+
+function getModifier(data) {
+    data ||= pf1.components.ItemConditionalModifier.defaultData;
+    let modifier = new pf1.components.ItemConditionalModifier(data);
+
+    modifier = { ...modifier, ...modifier.data };
+    modifier.id = modifier._id;
+
+    modifier.targets = getConditionalTargets();
+    modifier.subTargets = getConditionalSubTargets(modifier.target);
+    modifier.conditionalModifierTypes = getConditionalModifierTypes(modifier.target);
+    modifier.conditionalCritical = getConditionalCritical(modifier.target);
+
+    return modifier;
+}
+
+function getConditional({ data, modifiers } = {}) {
+    data ||= pf1.components.ItemConditional.defaultData;
+    const c = new pf1.components.ItemConditional(data);
+    c._id = c.id;
+
+    if (modifiers?.length) {
+        // c.data.modifiers = loadModifiers(modifiers);
+        c.modifiers = modifiers.map(({ data }) => getModifier(data));
+        c.data.modifiers = c.modifiers;
+    }
+    else {
+        c.modifiers = [];
+    }
+
+    return c;
+}
 
 /**
  * @param {object} args
@@ -45,129 +141,10 @@ export function conditionalsInput({
         // item.setFlag(MODULE_NAME, key, []);
     }
 
-    function getConditionalTargets() {
-        const result = {
-            attack: pf1.config.conditionalTargets.attack._label,
-            damage: pf1.config.conditionalTargets.damage._label,
-            size: pf1.config.conditionalTargets.size._label,
-            effect: pf1.config.conditionalTargets.effect._label,
-        };
-
-        // Only add Misc target if subTargets are available
-        if (Object.keys(getConditionalSubTargets("misc")).length > 0) {
-            result.misc = game.i18n.localize(pf1.config.conditionalTargets.misc._label);
-        }
-        return result;
-    }
-
-    /**
-     * Generates lists of conditional subtargets this attack can have.
-     *
-     * @param {string} target - The target key, as defined in PF1.conditionTargets.
-     * @returns {Object<string, string>} A list of conditionals
-     */
-    function getConditionalSubTargets(target) {
-        const result = {};
-        // Add static targets
-        if (hasProperty(pf1.config.conditionalTargets, target)) {
-            for (const [k, v] of Object.entries(pf1.config.conditionalTargets[target])) {
-                if (!k.startsWith("_") && !k.startsWith("~")) result[k] = v;
-            }
-        }
-        // Add subtargets depending on attacks
-        if (["attack", "damage"].includes(target)) {
-            // Add specific attacks
-            result["attack_0"] = `${game.i18n.localize("PF1.Attack")} 1`;
-            // for (const [k, v] of Object.entries(this.data.attackParts)) {
-            //   result[`attack_${Number(k) + 1}`] = v[1];
-            // }
-        }
-        // Add subtargets affecting effects
-        if (target === "effect") {
-            result["dc"] = game.i18n.localize("PF1.DC");
-        }
-        // Add misc subtargets
-        if (target === "misc") {
-            // Add charges subTarget with specific label
-            result["charges"] = game.i18n.localize("PF1.ChargeCost");
-        }
-        return result;
-    }
-
-    /** Generates lists of conditional modifier bonus types applicable to a formula.
-     * @param {string} target - The target key as defined in PF1.conditionTargets.
-     * @returns {Object.<string, string>} A list of bonus types.
-     */
-    function getConditionalModifierTypes(target) {
-        const result = {};
-        if (target === "attack" || target === "damage") {
-            // Add bonusModifiers from PF1.bonusModifiers
-            for (const [k, v] of Object.entries(pf1.config.bonusModifiers)) {
-                result[k] = v;
-            }
-        }
-        if (target === "damage") {
-            for (const damageType of pf1.registry.damageTypes) {
-                result[damageType.id] = damageType.name;
-            }
-        }
-        return result;
-    }
-
-    /* Generates a list of critical applications for a given formula target.
-     * @param {string} target - The target key as defined in PF1.conditionalTargets.
-     * @returns {Object.<string, string>} A list of critical applications.
-     */
-    function getConditionalCritical(target) {
-        let result = {};
-        // Attack bonuses can only apply as critical confirm bonus
-        if (target === "attack") {
-            result = { ...result, normal: "PF1.Normal", crit: "PF1.CriticalConfirmBonus" };
-        }
-        // Damage bonuses can be multiplied or not
-        if (target === "damage") {
-            result = {
-                ...result,
-                normal: "PF1.Normal",
-                crit: "PF1.CritDamageBonusFormula",
-                nonCrit: "PF1.NonCritDamageBonusFormula",
-            };
-        }
-        return result;
-    }
-
-    function getModifier(data) {
-        data ||= pf1.components.ItemConditionalModifier.defaultData;
-        const modifier = new pf1.components.ItemConditionalModifier(data);
-        modifier._id = modifier.id;
-
-        modifier.targets = getConditionalTargets();
-        modifier.subTargets = getConditionalSubTargets(modifier.target);
-        modifier.conditionalModifierTypes = getConditionalModifierTypes(modifier.target);
-        modifier.conditionalCritical = getConditionalCritical(modifier.target);
-
-        return modifier;
-    }
-
-    function getConditional({ data, modifiers } = {}) {
-        data ||= pf1.components.ItemConditional.defaultData;
-        const c = new pf1.components.ItemConditional(data);
-        c._id = c.id;
-
-        if (modifiers?.length) {
-            // c.data.modifiers = loadModifiers(modifiers);
-            c.modifiers = modifiers.map(({ data }) => getModifier(data));
-        }
-        else {
-            c.modifiers = [];
-        }
-
-        return c;
-    }
-
     /** @type {ItemConditional[]} */
     let conditionals = (item.getFlag(MODULE_NAME, key) || []).map(getConditional);
     const templateData = {
+        damageTypes: pf1.registry.damageTypes.toObject(),
         data: {
             conditionals,
         }
@@ -176,18 +153,18 @@ export function conditionalsInput({
     const div = document.createElement('div');
     div.innerHTML = conditionalsInput;
 
+    async function updateItem() {
+        const sanitized = conditionals.map((c) => ({
+            data: c.data,
+            modifiers: c.modifiers.map(({ data }) => ({ data })),
+        }));
+        await item.setFlag(MODULE_NAME, key, sanitized);
+    }
+
     div.querySelectorAll('.conditional-control').forEach((element) => {
         element.addEventListener('click', async (event) => {
             event.preventDefault();
             const a = event.currentTarget;
-
-            async function updateItem() {
-                const sanitized = conditionals.map((c) => ({
-                    data: c.data,
-                    modifiers: c.modifiers.map(({ data }) => ({ data })),
-                }));
-                await item.setFlag(MODULE_NAME, key, conditionals);
-            }
 
             // Add new conditional
             if (a.classList.contains("add-conditional")) {
@@ -218,6 +195,76 @@ export function conditionalsInput({
                 await updateItem();
             }
         });
+    });
+
+    function handleChangeForSelector(selector) {
+        div.querySelectorAll(selector).forEach((element) => {
+            element?.addEventListener(
+                'change',
+                async (event) => {
+                    const props = element.name.split('.');
+                    props.shift();
+                    props.splice(props.length - 1, 0, 'data');
+                    const path = props.join('.');
+                    const value = event.target?.value;
+                    setProperty(conditionals, path, value);
+                    await updateItem();
+                },
+            );
+        });
+    }
+    [
+        '.conditionals input[type=text]',
+        '.conditionals select',
+    ].forEach(handleChangeForSelector);
+
+    div.querySelectorAll('.damage-type-visual').forEach((element) => {
+        element?.addEventListener(
+            'click',
+            async (event) => {
+                event.preventDefault();
+                const clickedElement = event.currentTarget;
+                let props = element.getAttribute('data-name')?.split('.');
+                if (!props) {
+                    const parent = element.parentElement.parentElement.querySelector('.conditional-target');
+                    props = parent.name.split('.');
+                    props.pop();
+                    props.push('damageType');
+                }
+                props.shift();
+                props.splice(props.length - 1, 0, 'data');
+                const path = props.join('.');
+
+                // // Check for normal damage part
+                // const damageIndex = clickedElement.closest(".damage-part")?.dataset.damagePart;
+                // const damagePart = clickedElement.closest(".damage")?.dataset.key;
+                // if (damageIndex != null && damagePart != null) {
+                async function update( /** @type {{ [key: string]: object }} */arg) {
+                    setProperty(conditionals, path, arg[path]);
+                    await updateItem();
+                }
+                const app = new pf1.applications.DamageTypeSelector(
+                    {
+                        id: key,
+                        update,
+                    },
+                    path,
+                    getProperty(conditionals, path)
+                );
+                return app.render(true);
+                // }
+
+                // Check for conditional
+                const conditionalElement = clickedElement.closest(".conditional");
+                const modifierElement = clickedElement.closest(".conditional-modifier");
+                if (conditionalElement && modifierElement) {
+                    const conditional = conditionals.find(({ id }) => id === conditionalElement.dataset.conditional);
+                    const modifier = conditional.modifiers.find(({ id }) => id === modifierElement.dataset.modifier);
+                    const app = new pf1.applications.DamageTypeSelector(modifier, "damageType", modifier.data.damageType);
+                    return app.render(true);
+                }
+            },
+        );
     });
 
     addNodeToRollBonus(parentElement, div);
