f17eb9ff567879652c19b710bd9f04d3eb9d5cfb
dmrickey <dmrickey@gmail.com>
2024-08-16 14:54:32 -0500

    racial weapon focus

--- src/bonuses/weapon-focus/racial-weapon-focus.mjs
+++ src/bonuses/weapon-focus/racial-weapon-focus.mjs
@@ -1,10 +1,11 @@
 import { MODULE_NAME } from "../../consts.mjs";
 import { textInput } from "../../handlebars-handlers/bonus-inputs/text-input.mjs";
-import { KeyedDFlagHelper } from "../../util/flag-helpers.mjs";
-import { customGlobalHooks } from "../../util/hooks.mjs";
+import { intersects } from '../../util/array-intersects.mjs';
+import { LocalHookHandler, customGlobalHooks, localHooks } from "../../util/hooks.mjs";
 import { registerItemHint } from "../../util/item-hints.mjs";
 import { localize, localizeBonusLabel } from "../../util/localize.mjs";
 import { registerSetting } from "../../util/settings.mjs";
+import { truthiness } from '../../util/truthiness.mjs';
 import { SpecificBonuses } from '../all-specific-bonuses.mjs';
 import { gnomeWeaponFocusId, racialWeaponFocusKey, weaponFocusKey } from "./ids.mjs";
 
@@ -12,7 +13,7 @@ const key = racialWeaponFocusKey;
 const journal = 'Compendium.ckl-roll-bonuses.roll-bonuses-documentation.JournalEntry.FrG2K3YAM1jdSxcC.JournalEntryPage.ez01dzSQxPTiyXor#weapon-focus';
 
 Hooks.once('ready', () =>
-    SpecificBonuses.registerSpecificBonus({ journal, key: racialWeaponFocusKey, parent: weaponFocusKey })
+    SpecificBonuses.registerSpecificBonus({ journal, key: racialWeaponFocusKey, parent: weaponFocusKey, type: 'boolean' })
 );
 
 class Settings {
@@ -20,6 +21,7 @@ class Settings {
 
     static get racialWeaponFocus() { return Settings.#getSetting(key); }
     static get defaultRace() { return Settings.#getSetting(this.defaultRaceKey); }
+    // This is a per-client string which is different than all the other language settings
     // @ts-ignore
     static #getSetting(/** @type {string} */key) { return game.settings.get(MODULE_NAME, key).toLowerCase(); }
 
@@ -29,9 +31,37 @@ class Settings {
     }
 }
 
+/**
+ * @param {ActorPF} actor
+ * @param {ItemPF} item
+ * @returns {boolean}
+ */
+const isItemFocused = (actor, item) => {
+    const tags = (item.system.tags || []).map((tag) => tag.toLocaleLowerCase());
+    const values = (actor[MODULE_NAME][key] || [])
+        .map(x => x.getFlag(MODULE_NAME, key))
+        .filter(truthiness)
+        .map(x => x.toLocaleLowerCase());
+    return intersects(tags, values);
+}
+
+/**
+ * @param {ItemPF} item
+ * @param {RollData} _rollData
+ */
+function prepareData(item, _rollData) {
+    if (!item?.actor || !item.isActive) return;
+
+    if (item.hasItemBooleanFlag(key)) {
+        item.actor[MODULE_NAME][key] ||= [];
+        item.actor[MODULE_NAME][key].push(item);
+    }
+}
+LocalHookHandler.registerHandler(localHooks.prepareData, prepareData);
+
 // register hint on source
 registerItemHint((hintcls, _actor, item, _data) => {
-    const current = item.getItemDictionaryFlag(key);
+    const current = item.getFlag(MODULE_NAME, key);
     if (!current) {
         return;
     }
@@ -44,17 +74,10 @@ registerItemHint((hintcls, _actor, item, _data) => {
 
 // register hint on focused weapon/attack
 registerItemHint((hintcls, actor, item, _data) => {
-    if (!['attack', 'weapon'].includes(item.type)) return;
+    if (!['attack', 'weapon'].includes(item.type) || !actor) return;
 
-    const tags = (item.system.tags || []).map((tag) => tag.toLocaleLowerCase());
-
-    const helper = new KeyedDFlagHelper(actor, {
-        mustHave: {
-            [key]: (value) => tags.includes(`${value}`.toLocaleLowerCase()),
-        }
-    }, key);
-
-    if (helper.hasAnyFlags()) {
+    const isFocused = isItemFocused(actor, item);
+    if (isFocused) {
         const label = localize(`settings.${key}.name`);
         return hintcls.create(label, [], {});
     }
@@ -70,15 +93,8 @@ function getAttackSources(item, sources) {
     const actor = item.actor;
     if (!actor) return sources;
 
-    const tags = (item.system.tags || []).map((tag) => tag.toLocaleLowerCase());
-
-    const helper = new KeyedDFlagHelper(actor, {
-        mustHave: {
-            [key]: (value) => tags.includes(`${value}`.toLocaleLowerCase()),
-        }
-    }, key);
-
-    if (helper.hasAnyFlags()) {
+    const isFocused = isItemFocused(actor, item);
+    if (isFocused) {
         sources.push({ value: 1, name: localizeBonusLabel(weaponFocusKey), modifier: 'untyped', sort: -100 });
         return sources.sort((a, b) => b.sort - a.sort);
     }
@@ -94,15 +110,8 @@ Hooks.on(customGlobalHooks.itemGetAttackSources, getAttackSources);
 function addWeaponFocusBonus({ actor, item, shared }) {
     if (!actor || !item?.system.tags?.length) return;
 
-    const tags = (item.system.tags || []).map((tag) => tag.toLocaleLowerCase());
-
-    const helper = new KeyedDFlagHelper(actor, {
-        mustHave: {
-            [key]: (value) => tags.includes(`${value}`.toLocaleLowerCase()),
-        }
-    }, key);
-
-    if (helper.hasAnyFlags()) {
+    const isFocused = isItemFocused(actor, item);
+    if (isFocused) {
         shared.attackBonus.push(`${1}[${localizeBonusLabel(weaponFocusKey)}]`);
     }
 }
@@ -117,14 +126,14 @@ Hooks.on('renderItemSheet', (
 
     const name = item?.name?.toLowerCase() ?? '';
     const sourceId = item?.flags.core?.sourceId ?? '';
-    const isRacial = sourceId.includes(gnomeWeaponFocusId)
-        || item.system.flags.dictionary.hasOwnProperty(key)
+    const isRacial = item.hasItemBooleanFlag(key)
+        || sourceId.includes(gnomeWeaponFocusId)
         || name.includes(Settings.defaultRace);
     if (!isRacial) return;
 
-    const current = item.system?.flags?.dictionary?.[key];
+    const current = item.getFlag(MODULE_NAME, key);
     if (!current) {
-        item.setItemDictionaryFlag(key, Settings.defaultRace);
+        item.setFlag(MODULE_NAME, key, Settings.defaultRace);
     }
 
     textInput({
@@ -136,5 +145,6 @@ Hooks.on('renderItemSheet', (
     }, {
         canEdit: isEditable,
         isFormula: false,
+        isModuleFlag: true,
     });
 });
