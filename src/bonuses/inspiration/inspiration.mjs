23ee934d92c783e007126f4d8ad570363a0d75a1
dmrickey <dmrickey@gmail.com>
2025-05-10 17:10:27 -0500

    Updated inspiration

--- src/bonuses/inspiration/inspiration.mjs
+++ src/bonuses/inspiration/inspiration.mjs
@@ -6,16 +6,48 @@ import { onCreate } from '../../util/on-create.mjs';
 import { SpecificBonus } from '../_specific-bonus.mjs';
 import { allKnowledges, getSkillChoices, getSkillHints } from '../../util/get-skills.mjs';
 import { LanguageSettings } from '../../util/settings.mjs';
-
-const compendiumId = 'nKbyztRQCU5XMbbs';
-const journal = 'Compendium.ckl-roll-bonuses.roll-bonuses-documentation.JournalEntry.FrG2K3YAM1jdSxcC.JournalEntryPage.ez01dzSQxPTiyXor#inspiration';
+import { showEnabledLabel } from '../../handlebars-handlers/enabled-label.mjs';
 
 export class Inspiration extends SpecificBonus {
     /** @inheritdoc @override */
     static get sourceKey() { return 'inspiration'; }
 
     /** @inheritdoc @override */
-    static get journal() { return journal; }
+    static get journal() { return 'Compendium.ckl-roll-bonuses.roll-bonuses-documentation.JournalEntry.FrG2K3YAM1jdSxcC.JournalEntryPage.ez01dzSQxPTiyXor#inspiration'; }
+
+    /** @inheritdoc @override @returns {CreateAndRender} */
+    static get configuration() {
+        return {
+            type: 'render-and-create',
+            compendiumId: 'nKbyztRQCU5XMbbs',
+            isItemMatchFunc: (name, item) => name === Settings.name && !!item?.system.sources?.find(({ id, pages }) => id === 'PZO1129' && pages == 31),
+            itemFilter: (item) => item instanceof pf1.documents.item.ItemPF,
+            showInputsFunc: (item, html, isEditable) => {
+                const choices = getSkillChoices(item.actor, { isEditable });
+
+                traitInput({
+                    choices,
+                    hasCustom: false,
+                    item,
+                    journal: this.journal,
+                    key: this.key,
+                    parent: html,
+                }, {
+                    canEdit: isEditable,
+                    inputType: 'specific-bonus',
+                });
+            },
+            options: {
+                defaultFlagValuesFunc: () => ({
+                    [Inspiration.key]: [
+                        allKnowledges,
+                        'lin',
+                        'spl',
+                    ]
+                })
+            },
+        };
+    }
 }
 class Settings {
     static get inpsiration() { return LanguageSettings.getTranslation(Inspiration.sourceKey); }
@@ -41,53 +73,3 @@ registerItemHint((hintcls, actor, item, _data) => {
     const hint = hintcls.create('', [], { hint: hintText, icon: 'fas fa-magnifying-glass' });
     return hint;
 });
-
-Hooks.on('renderItemSheet', (
-    /** @type {ItemSheetPF} */ { isEditable, item },
-    /** @type {[HTMLElement]} */[html],
-    /** @type {unknown} */ _data
-) => {
-    if (!(item instanceof pf1.documents.item.ItemPF)) return;
-
-    const hasFlag = item.hasItemBooleanFlag(Inspiration.key);
-    if (!hasFlag) {
-        const name = item?.name?.toLowerCase() ?? '';
-        const hasCompendiumId = itemHasCompendiumId(item, compendiumId);
-        if (isEditable && (name === Settings.inpsiration || hasCompendiumId)) {
-            item.addItemBooleanFlag(Inspiration.key);
-        }
-        return;
-    }
-
-    const choices = getSkillChoices(item.actor, { isEditable });
-
-    traitInput({
-        choices,
-        hasCustom: false,
-        item,
-        journal,
-        key: Inspiration.key,
-        parent: html,
-    }, {
-        canEdit: isEditable,
-        inputType: 'specific-bonus',
-    });
-});
-
-onCreate(
-    compendiumId,
-    () => Settings.inpsiration,
-    {
-        booleanKeys: Inspiration.key,
-        // intentionally using `==` here
-        extraVerification: (item) => !!item.system.sources?.find(({ id, pages }) => id === 'PZO1129' && pages == 31),
-        flagValues: {
-            [Inspiration.key]: /** @type {SkillId[]} */
-                ([
-                    allKnowledges,
-                    'lin',
-                    'spl',
-                ]),
-        }
-    },
-);
