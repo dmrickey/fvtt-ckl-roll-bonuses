e08d505839c332b7bb78691560ca5f38fe949512
dmrickey <dmrickey@gmail.com>
2026-01-21 12:13:08 -0600

    fixed v12 compatibility

--- src/handlebars-handlers/init.mjs
+++ src/handlebars-handlers/init.mjs
@@ -1,3 +1,11 @@
 import { templates } from "./templates.mjs";
 
-Hooks.on('ready', () => foundry.applications.handlebars.loadTemplates(Object.values(templates)));
+Hooks.on('ready', () => {
+    // @ts-ignore
+    if (game.release.generation === 12) {
+        loadTemplates(Object.values(templates));
+    }
+    else {
+        Hooks.on('ready', () => foundry.applications.handlebars.loadTemplates(Object.values(templates)));
+    }
+});
